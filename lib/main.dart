fa3a1cddfef4d6d3bf3dae5e801267d6ebb9b93f
sazarcode <cerberusprogrammer@gmail.com>
2023-05-12 21:35:55 -0700

    Plato del bien comer creado.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,86 +19,124 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
+  const MyHomePage({super.key, required this.title});
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int tappedSection = -1;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              double size =
+                  math.min(constraints.maxWidth, constraints.maxHeight);
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  GestureDetector(
+                    onTapDown: (details) {
+                      setState(() {
+                        tappedSection =
+                            getTappedSection(details.localPosition, size);
+                      });
+                    },
+                    onTapUp: (details) {
+                      setState(() {
+                        tappedSection = -1;
+                      });
+                    },
+                    child: CustomPaint(
+                      size: Size(size, size),
+                      painter: CirclePainter(tappedSection),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  int getTappedSection(Offset tapPosition, double size) {
+    double angle =
+        math.atan2(tapPosition.dy - size / 2, tapPosition.dx - size / 2);
+    if (angle < 0) angle += 2 * math.pi;
+    return (angle / (2 * math.pi / 5)).floor();
+  }
+}
+
+class CirclePainter extends CustomPainter {
+  final int tappedSection;
+  final List<Color> sectionColors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue
+  ];
+  final List<Color> sectionPressedColors = [
+    Colors.pink,
+    Colors.deepOrange,
+    Colors.amber,
+    Colors.lightGreen,
+    Colors.lightBlue
+  ];
+
+  CirclePainter(this.tappedSection);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()..style = PaintingStyle.fill;
+
+    for (int i = 0; i < 5; i++) {
+      double startAngle = i * (2 * math.pi / 5);
+      double endAngle = startAngle + (2 * math.pi / 5);
+      paint.color =
+          (i == tappedSection) ? sectionPressedColors[i] : sectionColors[i];
+      canvas.drawArc(
+          Rect.fromCircle(
+              center: Offset(size.width / 2, size.height / 2),
+              radius: size.width / 2),
+          startAngle,
+          endAngle - startAngle,
+          true,
+          paint);
+    }
+
+    paint
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10;
+
+    for (int i = 0; i < 5; i++) {
+      double startAngle = i * (2 * math.pi / 5);
+
+      // Dibujar línea desde el centro hasta el borde del círculo
+      double lineAngle = startAngle;
+      double x = size.width / 2 +
+          math.cos(lineAngle) * (size.width / 2 + paint.strokeWidth / 2);
+      double y = size.height / 2 +
+          math.sin(lineAngle) * (size.width / 2 + paint.strokeWidth / 2);
+      canvas.drawLine(
+          Offset(size.width / 2, size.height / 2), Offset(x, y), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
